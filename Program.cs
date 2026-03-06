af20dc34ea3956847007dfba1e0ebd96ce578a66
Pedro <pedrohenrique2007@proton.me>
2026-03-06 17:56:18 -0300

    No1000-No1010 Problems Solved

--- Program.cs
+++ Program.cs
@@ -4,6 +4,96 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // No1000
+        // Console.WriteLine("Hello World!");
+        
+        //No1001
+        // int a = int.Parse(Console.ReadLine());
+        // int b = int.Parse(Console.ReadLine());
+        // int x = a + b;
+        //
+        // Console.WriteLine($"X = {x}");
+        
+        //No1002
+        // double r = double.Parse(Console.ReadLine());
+        // double area = 3.14159 * r * r;
+        //
+        // Console.WriteLine($"A={area:F4}");
+        
+        //No1003
+        // int a = int.Parse(Console.ReadLine());
+        // int b = int.Parse(Console.ReadLine());
+        //
+        // int soma = a + b;
+        //
+        // Console.WriteLine($"SOMA = {soma}");
+        
+        //No1004
+        // int a = int.Parse(Console.ReadLine());
+        // int b = int.Parse(Console.ReadLine());
+        //
+        // int produto = a * b;
+        //
+        // Console.WriteLine($"PROD = {produto}");
+        
+        //No1005
+        // double a = double.Parse(Console.ReadLine());
+        // double b = double.Parse(Console.ReadLine());
+        //
+        // double media = (a * 3.5 + b * 7.5) / 11;
+        //
+        // Console.WriteLine($"MEDIA = {media:F5}");
+        
+        //No1006
+        // double a = double.Parse(Console.ReadLine());
+        // double b = double.Parse(Console.ReadLine());
+        // double c = double.Parse(Console.ReadLine());
+        //
+        // double media = (a * 2 + b * 3 + c * 5) / 10;
+        //
+        // Console.WriteLine($"MEDIA = {media:F1}");
+        
+        //No1007
+        // int a = int.Parse(Console.ReadLine());
+        // int b = int.Parse(Console.ReadLine());
+        // int c = int.Parse(Console.ReadLine());
+        // int d = int.Parse(Console.ReadLine());
+        //
+        // int diferenca = a * b - c * d;
+        //
+        // Console.WriteLine($"DIFERENCA = {diferenca}");
+        
+        //No1008
+        // int numero = int.Parse(Console.ReadLine());
+        // int horas = int.Parse(Console.ReadLine());
+        // double valor = double.Parse(Console.ReadLine());
+        //
+        // double salario = horas * valor;
+        //
+        // Console.WriteLine($"NUMBER = {numero}");
+        // Console.WriteLine($"SALARY = U$ {salario:F2}");
+        
+        //No1009
+        // string nome = Console.ReadLine();
+        // double salarioFixo = double.Parse(Console.ReadLine());
+        // double vendas = double.Parse(Console.ReadLine());
+        //
+        // double salario = salarioFixo + vendas * 0.15;
+        //
+        // Console.WriteLine($"TOTAL = R$ {salario:F2}");
+        
+        //No1010
+        // string linha1 = Console.ReadLine();
+        // string linha2 = Console.ReadLine();
+        //
+        // string[] dadosPeca1 = linha1.Split(' ');
+        // string[] dadosPeca2 = linha2.Split(' ');
+        //
+        // double valorTotal = int.Parse(dadosPeca1[1]) * double.Parse(dadosPeca1[2]) + int.Parse(dadosPeca2[1]) * double.Parse(dadosPeca2[2]);
+        //
+        // Console.WriteLine($"VALOR A PAGAR: R$ {valorTotal:F2}");
+        
+        //No1011
+        
     }
 }
